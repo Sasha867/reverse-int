0e44b65b5ce10f965c99277bf785454ba1d5fbee
Sasha867 <sashacordoba@mail.ru>
2023-02-25 16:28:09 +0300

    feat: add function reversr

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const needNumber = Math.abs(n);
+    const str = String(needNumber);
+    const needStr = str.split("").reverse().join("");
+    return Number(needStr);
+};
